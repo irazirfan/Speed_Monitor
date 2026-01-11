06e3d474aaa558611ff3dee4148fb76b5538b0c6
irazirfan <irazirfan@gmail.com>
2026-01-11 20:37:54 +0600

    fixed to show in bottom right

--- iONE_Speed_Monitor/NetworkSpeedForm.vb
+++ iONE_Speed_Monitor/NetworkSpeedForm.vb
@@ -6,63 +6,84 @@ Public Class NetworkSpeedForm
     Inherits Form
 
     Private WithEvents Timer1 As Timer
-    Private prevBytesReceived As Long = 0
-    Private prevBytesSent As Long = 0
+    Private prevBytesReceived As Long
+    Private prevBytesSent As Long
 
-    Private drag As Boolean = False
-    Private offset As Point
+    ' Drag support
+    Private dragging As Boolean = False
+    Private dragOffset As Point
+
+    ' Context menu
+    Private contextMenu As ContextMenuStrip
 
     Public Sub New()
+        ' ---------------- Form setup ----------------
         Me.FormBorderStyle = FormBorderStyle.None
         Me.TopMost = True
-        Me.Width = 220
-        Me.Height = 50
-        Me.BackColor = Color.Black
         Me.ShowInTaskbar = False
+        Me.Width = 155
+        Me.Height = 48
+        Me.BackColor = Color.Black
+        Me.DoubleBuffered = True
 
-        ' Position near bottom-right
-        Dim bounds = Screen.PrimaryScreen.WorkingArea
-        Me.Location = New Point(bounds.Right - Me.Width - 10, bounds.Bottom - Me.Height - 10)
+        Me.StartPosition = FormStartPosition.Manual
+        ' Position window at bottom-right (above taskbar)
+        Dim bounds As Rectangle = Screen.PrimaryScreen.WorkingArea
+        Me.Location = New Point(
+            bounds.Right - Me.Width - 0,
+            bounds.Bottom - Me.Height - 0
+        )
 
-        ' Draggable
-        AddHandler Me.MouseDown, Sub(s, e)
-                                     If e.Button = MouseButtons.Left Then
-                                         drag = True
-                                         offset = e.Location
-                                     End If
-                                 End Sub
-        AddHandler Me.MouseMove, Sub(s, e)
-                                     If drag Then
-                                         Me.Location = New Point(Me.Left + e.X - offset.X, Me.Top + e.Y - offset.Y)
-                                     End If
-                                 End Sub
-        AddHandler Me.MouseUp, Sub(s, e)
-                                   drag = False
-                               End Sub
+        ' ---------------- Context Menu ----------------
+        contextMenu = New ContextMenuStrip()
+        Dim exitItem As New ToolStripMenuItem("Exit")
+        AddHandler exitItem.Click, AddressOf ExitApp
+        contextMenu.Items.Add(exitItem)
+        Me.ContextMenuStrip = contextMenu
 
-        ' Initialize Timer
+        ' ---------------- Drag handlers ----------------
+        AddHandler Me.MouseDown, AddressOf Form_MouseDown
+        AddHandler Me.MouseMove, AddressOf Form_MouseMove
+        AddHandler Me.MouseUp, AddressOf Form_MouseUp
+
+        ' ---------------- Timer ----------------
         Timer1 = New Timer()
         Timer1.Interval = 1000
         AddHandler Timer1.Tick, AddressOf Timer1_Tick
         Timer1.Start()
 
-        ' Initialize previous bytes
-        Dim nic = NetworkInterface.GetAllNetworkInterfaces() _
-            .FirstOrDefault(Function(n) n.OperationalStatus = OperationalStatus.Up AndAlso n.NetworkInterfaceType <> NetworkInterfaceType.Loopback)
+        ' ---------------- Init counters ----------------
+        Dim nic = GetActiveNIC()
         If nic IsNot Nothing Then
             Dim stats = nic.GetIPv4Statistics()
             prevBytesReceived = stats.BytesReceived
             prevBytesSent = stats.BytesSent
         End If
 
-        ' Force initial draw
         Me.Tag = New Tuple(Of Double, Double)(0, 0)
-        Me.Invalidate()
     End Sub
 
+    ' ---------------- Drag logic ----------------
+    Private Sub Form_MouseDown(sender As Object, e As MouseEventArgs)
+        If e.Button = MouseButtons.Left Then
+            dragging = True
+            dragOffset = e.Location
+        End If
+    End Sub
+
+    Private Sub Form_MouseMove(sender As Object, e As MouseEventArgs)
+        If dragging Then
+            Me.Location = New Point(Me.Left + e.X - dragOffset.X, Me.Top + e.Y - dragOffset.Y)
+        End If
+    End Sub
+
+    Private Sub Form_MouseUp(sender As Object, e As MouseEventArgs)
+        dragging = False
+    End Sub
+
+    ' ---------------- Timer update ----------------
     Private Sub Timer1_Tick(sender As Object, e As EventArgs)
-        Dim nic = NetworkInterface.GetAllNetworkInterfaces() _
-            .FirstOrDefault(Function(n) n.OperationalStatus = OperationalStatus.Up AndAlso n.NetworkInterfaceType <> NetworkInterfaceType.Loopback)
+        Dim nic = GetActiveNIC()
         If nic Is Nothing Then Return
 
         Dim stats = nic.GetIPv4Statistics()
@@ -76,29 +97,39 @@ Public Class NetworkSpeedForm
         Me.Invalidate()
     End Sub
 
+    ' ---------------- Paint ----------------
     Protected Overrides Sub OnPaint(e As PaintEventArgs)
         MyBase.OnPaint(e)
         Dim g = e.Graphics
-        g.Clear(Me.BackColor)
+        g.Clear(Color.Black)
         g.TextRenderingHint = Drawing.Text.TextRenderingHint.ClearTypeGridFit
 
-        Dim speeds = TryCast(Me.Tag, Tuple(Of Double, Double))
-        If speeds Is Nothing Then speeds = New Tuple(Of Double, Double)(0, 0)
+        Dim speeds = CType(Me.Tag, Tuple(Of Double, Double))
 
         Using font As New Font("Consolas", 14, FontStyle.Bold)
             g.DrawString($"↓ {FormatSpeed(speeds.Item1)}", font, Brushes.Lime, 10, 5)
-            g.DrawString($"↑ {FormatSpeed(speeds.Item2)}", font, Brushes.Cyan, 10, 25)
+            g.DrawString($"↑ {FormatSpeed(speeds.Item2)}", font, Brushes.Cyan, 10, 30)
         End Using
     End Sub
 
+    ' ---------------- Helpers ----------------
     Private Function FormatSpeed(bytesPerSec As Double) As String
-        Dim speed = bytesPerSec
-        If speed < 1024 Then Return $"{speed:0} B/s"
-        speed /= 1024
-        If speed < 1024 Then Return $"{speed:0.0} KB/s"
-        speed /= 1024
-        Return $"{speed:0.00} MB/s"
+        If bytesPerSec < 1024 Then Return $"{bytesPerSec:0} B/s"
+        bytesPerSec /= 1024
+        If bytesPerSec < 1024 Then Return $"{bytesPerSec:0.0} KB/s"
+        bytesPerSec /= 1024
+        Return $"{bytesPerSec:0.00} MB/s"
     End Function
+
+    Private Function GetActiveNIC() As NetworkInterface
+        Return NetworkInterface.GetAllNetworkInterfaces().
+            FirstOrDefault(Function(n) n.OperationalStatus = OperationalStatus.Up AndAlso
+                                       n.NetworkInterfaceType <> NetworkInterfaceType.Loopback)
+    End Function
+
+    ' ---------------- Exit ----------------
+    Private Sub ExitApp(sender As Object, e As EventArgs)
+        Timer1.Stop()
+        Application.Exit()
+    End Sub
 End Class
-
-
